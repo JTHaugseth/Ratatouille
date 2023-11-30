7f7b48a774d325b0c756204343546f1ed81262f5
JTHaugseth <97683128+JTHaugseth@users.noreply.github.com>
2023-11-30 18:36:58 +0100

    Update

--- Ratatouille/Views/SearchView.swift
+++ Ratatouille/Views/SearchView.swift
@@ -6,14 +6,188 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct SearchView: View {
+    
+    @Query(filter: #Predicate<AreaDbModel>{$0.archived == false},
+           sort: \AreaDbModel.title, order: .forward, animation: .default) private var savedAreas: [AreaDbModel]
+    
+    @Query(filter: #Predicate<CategoryDbModel>{$0.archived == false},
+           sort: \CategoryDbModel.title, order: .forward, animation: .default) private var savedCategories: [CategoryDbModel]
+    
+    @Query(filter: #Predicate<IngredientDbModel>{$0.archived == false},
+           sort: \IngredientDbModel.title, order: .forward, animation: .default) private var savedIngredients: [IngredientDbModel]
+    
+    @State private var selectedButtonType: SelectedButtonType? = nil
+    @State private var selectedItemTitle: String = ""
+    @State private var showDetails = false
+
     var body: some View {
-        // Sett opp søkevisningen her
-        Text("Søkefunksjonalitet ikke implementert ennå")
+        VStack {
+            HStack {
+                // Content List
+                if !showDetails {
+                    switch selectedButtonType {
+                    case .area:
+                        areaList()
+                    case .category:
+                        categoryList()
+                    case .ingredient:
+                        ingredientList()
+                    default:
+                        Text("Søk etter matoppskrifter")
+                    }
+                }
+
+                // Details View
+                if showDetails {
+                    mealListView()
+                        .transition(.move(edge: .trailing))
+                }
+            }
+            .animation(.default, value: showDetails)
+
+            // Bottom bar with buttons
+            HStack {
+                Spacer()
+
+                Button(action: {
+                    selectedButtonType = .area
+                    showDetails = false
+                }) {
+                    buttonView(systemName: "globe", color: .blue)
+                }
+
+                Spacer()
+
+                Button(action: {
+                    selectedButtonType = .category
+                    showDetails = false
+                }) {
+                    buttonView(systemName: "list.bullet", color: .green)
+                }
+
+                Spacer()
+
+                Button(action: {
+                    selectedButtonType = .ingredient
+                    showDetails = false
+                }) {
+                    buttonView(systemName: "leaf", color: .orange)
+                }
+
+                Spacer()
+
+                Button(action: {
+                    selectedButtonType = .search
+                    showDetails = false
+                }) {
+                    buttonView(systemName: "magnifyingglass", color: .red)
+                }
+
+                Spacer()
+            }
+            .padding(.bottom)
+        }
+        .animation(.default, value: selectedButtonType)
+    }
+
+    // Separate list functions for each type
+    private func areaList() -> some View {
+        List(savedAreas, id: \.self) { area in
+            Button(action: { selectItem(area.title) }) {
+                HStack {
+                    if let flagURL = URL(string: "https://flagsapi.com/\(area.countrycode)/flat/64.png") {
+                        AsyncImage(url: flagURL) { image in
+                            image.resizable()
+                        } placeholder: {
+                            Color.gray
+                        }
+                        .frame(width: 50, height: 50)
+                    } else {
+                        // Fallback if flag URL is not available
+                        Color.gray
+                            .frame(width: 50, height: 50)
+                    }
+
+                    Text(area.title)
+                }
+            }
+        }
+        .transition(.move(edge: .leading))
+    }
+
+    private func categoryList() -> some View {
+        List(savedCategories, id: \.self) { category in
+            Button(action: { selectItem(category.title) }) {
+                HStack {
+                    if let url = URL(string: category.thumb) {
+                        AsyncImage(url: url) { image in
+                            image.resizable()
+                        } placeholder: {
+                            Color.gray
+                        }
+                        .frame(width: 50, height: 50)
+                        .cornerRadius(25)
+                    } else {
+                        Color.gray // Fallback placeholder
+                            .frame(width: 50, height: 50)
+                            .cornerRadius(25)
+                    }
+
+                    Text(category.title)
+                }
+            }
+        }
+        .transition(.move(edge: .leading))
+    }
+
+    private func ingredientList() -> some View {
+        List(savedIngredients, id: \.self) { ingredient in
+            Button(action: { selectItem(ingredient.title) }) {
+                Text(ingredient.title)
+            }
+        }
+        .transition(.move(edge: .leading))
+    }
+
+    // Function to handle item selection
+    private func selectItem(_ title: String) {
+        selectedItemTitle = title
+        withAnimation {
+            showDetails = true
+        }
+    }
+
+    // Placeholder for the meal list view
+    private func mealListView() -> some View {
+        VStack {
+            Text("Meals based on \(selectedItemTitle)")
+            // Display meals based on selectedItemTitle
+            // Implement API call logic here
+            Button("Back") {
+                withAnimation {
+                    showDetails = false
+                }
+            }
+        }
+    }
+
+    // Helper function for button view
+    private func buttonView(systemName: String, color: Color) -> some View {
+        Image(systemName: systemName)
+            .foregroundColor(.white)
+            .padding()
+            .background(color)
+            .clipShape(Circle())
+            .shadow(radius: 5)
+    }
+    
+    enum SelectedButtonType {
+        case area, category, ingredient, search
     }
 }
-
 #Preview {
     SearchView()
 }
